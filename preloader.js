49aa804bc36a86d57cd14df1d6fd44fc569a8c45
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-12-17 10:48:24 -0300

    descomentado

--- preloader.js
+++ preloader.js
@@ -1,6 +1,6 @@
 
 var loader = document.getElementById("preloader");
 
-/* window.addEventListener("load", function(){
+window.addEventListener("load", function(){
     loader.style.display = "none";
-}) */
+})
